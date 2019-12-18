64763661eb4d2e48edae82a59cac4c43f7174dd6
Justin Chester <jchester@git.socs.uoguelph.ca>
2019-12-18 14:56:13 -0500

    Change from fake json-server api to json import

--- src/app/My Work Terms/workterms.component.ts
+++ src/app/My Work Terms/workterms.component.ts
@@ -1,6 +1,8 @@
 /* tslint:disable:ban-types */
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../http.service';
+import * as t from '../../../db.json';
+
 
 @Component({
   selector: 'app-list',
@@ -8,14 +10,14 @@ import { HttpService } from '../http.service';
   styleUrls: ['./workterms.component.scss']
 })
 export class WorktermsComponent implements OnInit {
-
   workTerms: WorkTermInformation[];
 
   constructor(private http: HttpService) { }
 
   ngOnInit() {
-    this.http.getWorkTerms().subscribe(data => {
-      this.workTerms = data as WorkTermInformation[];
-    });
+    // this.http.getWorkTerms().subscribe(data => {
+    //   this.workTerms = data as WorkTermInformation[];
+    // });
+    this.workTerms = t.workTerms;
   }
 }
