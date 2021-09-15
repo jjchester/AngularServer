2e187c6f06f614cfc9bf864372a4a51921fa4303
Justin Chester <slict43@icloud.com>
2021-09-15 00:31:13 -0400

    Restructure for 3 work terms, new images

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,6 @@ import {WorktermsComponent} from './My Work Terms/workterms.component';
 import {Wt1Component} from './My Work Terms/wt1/wt1.component';
 import {Wt2Component} from './My Work Terms/wt2/wt2.component';
 import {Wt3Component} from './My Work Terms/wt3/wt3.component';
-import {Wt4Component} from './My Work Terms/wt4/wt4.component';
-import {Wt5Component} from './My Work Terms/wt5/wt5.component';
 import {AboutmeComponent} from './aboutme/aboutme.component';
 
 const routes: Routes = [
@@ -15,8 +13,6 @@ const routes: Routes = [
   { path: 'WT1', component: Wt1Component },
   { path: 'WT2', component: Wt2Component },
   { path: 'WT3', component: Wt3Component },
-  { path: 'WT4', component: Wt4Component },
-  { path: 'WT5', component: Wt5Component },
   { path: 'aboutme', component: AboutmeComponent }
 ];
 
